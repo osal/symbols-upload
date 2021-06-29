09e72208a5bff56ef4cbef90d70ba5d0ff9b2287
Aleksey Osipov <aosipov@tradingview.com>
2021-06-29 17:35:44 +0300

    update entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-aws --version
-
-INTEGRATION_NAME=${GITHUB_REPOSITORY##*/}
-FINAL_NAME=${INTEGRATION_NAME}/${FILE}
-echo uploading $FINAL_NAME to $S3_BUCKET/$ENVIRONMENT/$FINAL_NAME
-aws s3 cp "${FINAL_NAME}" "${S3_BUCKET}/${ENVIRONMENT}/${FINAL_NAME}" --no-progress
