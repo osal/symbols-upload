5343592a7cbaa838dd43530122ce6c55c6c89651
Aleksey Osipov <1631589+osal@users.noreply.github.com>
2021-07-04 18:05:49 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 aws --version
 SYMBOL_FILES=$(ls symbols)
 INTEGRATION_NAME=${GITHUB_REPOSITORY##*/}
-for FILE in $SYMBOL_FILES do;
+for FILE in $SYMBOL_FILES
+do
     FINAL_NAME=${INTEGRATION_NAME}/${FILE}
     echo uploading symbols/$FILE to $S3_BUCKET/$ENVIRONMENT/$FINAL_NAME
     aws s3 cp "symbols/${FILE}" "${S3_BUCKET}/${ENVIRONMENT}/${FINAL_NAME}" --no-progress
